bd1722f5437507384978eb6cf11164e1efd5ae8b
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-08-12 09:59:35 -0500

    Create component for svg images.

---
+++ front-end/src/components/svgImage.jsx
@@ -0,0 +1,9 @@
+function Svg(props) {
+    return ( 
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="mx-1" viewBox="0 0 16 16">
+            <path d={ props.svgPath }/>
+        </svg>
+    );
+}
+
+export default Svg;
