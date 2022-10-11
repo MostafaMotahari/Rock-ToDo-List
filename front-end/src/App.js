fa0b4b080901b98d7e26de7d107759496593944c
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-10-11 13:12:09 -0500

    Creating components from raw html and css files.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ToDoCard from './components/todoCard';
+import Button from './components/button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <div className="row g-2 py-2">
+        {/* Todo cards */}
+        <ToDoCard />
+        {/* Add new task button */}
+        <Button 
+          isCollapseButton={ false }
+          buttonVariant="outline-primary"
+          otherClasses="rounded-circle position-fixed"
+        />
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
